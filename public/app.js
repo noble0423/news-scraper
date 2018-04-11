ea69f5f8aacffd30daafbaf3858e88010aed9aab
noble0423 <ryan.noble82@gmail.com>
2018-04-11 10:02:55 -0700

    articles are rendering on html correctly (i forgot to include a link on the html pg).

--- public/app.js
+++ public/app.js
@@ -1,9 +1,8 @@
 // Grabs the articles as JSON 
 $.getJSON("/articles", function(data) {
-    console.log(data);
     for (let i = 0; i < data.length; i++) {
-        // $("#articles").append(`<p data-id= ${ data[i]._id } > ${ data[i].title } <br /> ${ data[i].link } <br /> ${ data[i].image } <br /> ${ data[i].publishDate } </p>`);
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
-        console.log(data[i].title);
+        $("#articles").append(`<p data-id= ${ data[i]._id } > ${ data[i].title } <br /> ${ data[i].link } <br /> ${ data[i].image } <br /> ${ data[i].publishDate } </p>`);
+        // $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        // console.log(data[i].title);
     }
 });
