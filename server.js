cce50a61493300ab0ee2859e44208abee7b08bee
noble0423 <ryan.noble82@gmail.com>
2018-04-10 23:50:34 -0700

    scraping info correctly from popularmechanics.com. need to figure out handlebars and getting data into DB correctly.

--- server.js
+++ server.js
@@ -34,10 +34,72 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/week18Populater");
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    // useMongoClient: true
+});
 
 // Routes
+// A GET route for scraping the Popular Mechanics website
+app.get("/scrape", function(req, res) {
+    // First, we grab th body of the html with axios
+    axios.get("https://www.popularmechanics.com/").then(function(response) {
+        // Then we load that into Cheerio and save it to $ for a shorthand selector
+        var $ = cheerio.load(response.data);
 
+        // Now, we grab every and do the following:
+        $(".full-item ").each(function(i, element) {
+            // Save an empty result object
+            var result = {};
+
+            result.title = $(this)
+                .children(".full-item-content")
+                .children("a")
+                .text()
+                .replace("\n\t", "")
+                .replace("\n", "");
+            result.link = "https://www.popularmechanics.com" + $(this)
+                .children(".full-item-content")
+                .children("a")
+                .attr("href");
+            result.image = $(this)
+                .children("a")
+                .children("img")
+                .attr("data-src");
+            result.publishDate = $(this)
+                .children(".full-item-metadata")
+                .children("div")
+                .text()
+                .replace("\n\t\t\n\t\t", "")
+                .replace("\n\t", "");
+            // result.briefDesc = $(this)
+            //     .children(".full-item-content")
+            //     .children(".full-item-dek item-dek")
+            //     // .children("p")
+            //     .text();
+
+            console.log(result);
+            // Create a new Article using the `result` object built from scraping
+            // db.Article.create(result)
+            //     .then(function(dbArticle) {
+            //         // View the added result in the console
+            //         console.log(dbArticle);
+            //     })
+            //     .catch(function(err) {
+            //         // If an error occurred, send it to the client
+            //         return res.json(err);
+            //     });
+        });
+
+        // If we were able to successfully scrape and save an Article, send a message to the client
+        res.send("Scrape Complete");
+    });
+});
 
 
 
