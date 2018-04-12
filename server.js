552e9996502455f0978f71c6f0672b9de7c3506e
noble0423 <ryan.noble82@gmail.com>
2018-04-12 07:08:56 -0700

    trying to deploy, added 'process.env.port ||' to server.js.

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var cheerio = require("cheerio");
 // Initialize Express
 var app = express();
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
