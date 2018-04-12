bc491ef6ca930cfd0ed0281c0582093d032731ce
noble0423 <ryan.noble82@gmail.com>
2018-04-12 06:59:00 -0700

    uncommented line on server.js RE DB (attempting to deploy on heroku).

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
-    // useMongoClient: true
+    useMongoClient: true
 });
 
 // Routes
